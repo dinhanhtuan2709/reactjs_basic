bea8c293d7d97202b2b7667cb4093f60e8068a1f
dinhanhtuan2709 <dinhanhtuan2709@gmail.com>
2023-07-08 22:13:25 +0700

    Form in ReactJS

--- src/view/Example/MyComponent.js
+++ src/view/Example/MyComponent.js
@@ -3,40 +3,42 @@ import React from 'react';
 class MyComponent extends React.Component{
 
     state = {
-        Name: 'Tuan',
-        Age: '20'
+      firstName : "",
+      lastName : ""
     }
-    ChangAge = (event) =>{
+    handdleFirstName = (event) => {
         this.setState({
-            Age: event.target.value
+             firstName: event.target.value
         })
     }
-     ChangName = (event) =>{
+    handdleLastName = (event) => {
         this.setState({
-            Name: event.target.value
+             lastName: event.target.value
         })
     }
-    handdleButton = () =>{
-        alert('Operation is Excute')
+    handdleSubmit = (event) => {
+        event.preventDefault()
+        console.log('>>>>>> My data : ', this.state)
     }
+
     render(){
         return(
             <>
+        <form>
+            <label>First name: {this.state.firstName}</label><br/>
+            <input type="text"
+                value={this.state.firstName}
+                onChange={(event) => this.handdleFirstName(event)} /><br/>
 
-                <div className='Name'>
-                    <input value={this.state.Name} type='text' onChange={(event) => this.ChangName(event)}/>
-                    My name is : {this.state.Name}</div>
-
-                <div className='Age'>
-                    <input value={this.state.Age} type='text' onChange={(event) => this.ChangAge(event)}/>
-                        This year, I'm {this.state.Age} years old
-                    </div>
-                <div className='btn-click'>
-                    <button onClick={this.handdleButton}>
-                        Click here
-                    </button>
-                </div>
-                
+            <label>Last name:{this.state.lastName}</label><br/>
+            <input type="text" 
+                value={this.state.lastName}
+                onChange={(event) => this.handdleLastName(event)}/><br/>
+            <input type="submit"
+                onClick = {(event) => this.handdleSubmit(event)}
+            />
+        </form> 
+               
             </>
         )
     }
